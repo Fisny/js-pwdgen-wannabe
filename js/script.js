2d908916eecef85276130bbb0379360ff9271dd0
Fisny <bardi.fisniku@gmail.com>
2021-05-27 19:41:42 +0200

    java script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var a = prompt('inserisci Nome');
+var b = prompt('inserisci Cognome');
+var c = prompt('inserisci il tuo colore preferito');
+
+var totale = a+b+c;
+
+document.getElementById('risultato').innerHTML = totale + 21;
